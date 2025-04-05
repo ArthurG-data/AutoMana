d0c430210e3459c79e2bf40311dca48449c54bb5
messi08 <guillaume.arthur1@gmail.com>
2025-04-05 21:31:43 +1000

    start work on add card api

--- backend/routers/cards.py
+++ backend/routers/cards.py
@@ -51,6 +51,76 @@ def get_cards_info(conn: connection,
 
 @router.post('/', response_model=None)
 async def add_card(cards : List[CreateCard]):
+    query = """ WITH
+                ins_unique_card AS (
+                INSERT INTO unique_cards_ref (card_name, cmc, mana_cost, reserved)
+                VALUES (%s, %s, %s, %s)
+                ON CONFLICT (card_name) DO NOTING
+                RETURNING unique_card_id
+                ),
+                get_unique_card AS (
+                SELECT unique_card_id FROM ins_unique_card
+                UNION
+                SELECT unique_card_id FROM unique_cards_ref WHERE card_name = %s
+                ),
+                ins_border_color AS (
+                INSERT INTO  border_color_ref (border_color_name)
+                VALUES (%s)
+                ON CONFLICT (boder_color_name) DO NOTHING
+                RETURNING border_color_id
+                ),
+                get_border_id AS (
+                SELECT border_color_id FROM ins_border_color
+                UNION
+                SELECT border_color_id FROM border_color_ref WHERE border_color_ref = %s
+                ),
+                ins_rarity AS (
+                INSERT INTO rarities rarity
+                VALUES (%s)
+                ON CONFLICT (rarity_name) DO NOTHING
+                RETURNING rarity_id
+                ),
+                get_rarity_id AS (
+                SELECT rarity id from ins_rarity
+                UNION
+                SELECT rarity_id FROM rarities WHERE rarity_name = %s
+                ),
+                ins_artist_id AS (
+                INSERT INTO artists_ref (artist_name)
+                VALUES (%s)
+                ON CONFLICT (artist_name) DO NOTTHING   
+                RETURNING artist_id
+                ),
+                get_artist_id AS (
+                SELECT artist_id from ins_artist_id
+                UNION
+                SELECT artist_id from artists_ref WHERE artist_name = %s
+                ),
+                ins_frame_id AS (
+                INSERT INTO frames_ref frame_year
+                VALUES (%s)
+                ON CONFLICT (frame_year) DO NOTHING
+                RETURNING frame_id
+                ),
+                get_frame_id AS (
+                SELECT frame_id FROM ins_frame_id
+                UNION
+                SELECT frame_id from frames_ref WHERE frame_year = %s
+                ),
+                ins_layout_id AS (
+                INSERT INTO layout_ref layout_name
+                VALUES (%s)
+                ON CONFLICT (layout_name) DO NOTHING
+                RETURNING layout_id 
+                ),
+                get_layout_id AS (
+                SELECT layout_id FROM ins_layout_id
+                UNION
+                SELECT layout_id FROM layout_ref WHERE layout_name = %s
+                ),
+                ins
+
+    """
     return {"received": len(cards), "cards": cards}
  
 @router.get('/{card_id}', response_model=BaseCard)
