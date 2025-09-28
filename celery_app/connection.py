30d6ab06503d766a02dd5bee241c2857aa74d95d
messi08 <guillaume.arthur1@gmail.com>
2025-09-28 22:20:09 +1000

    feat: add database schemas and connection management for Scryfall integration; implement tasks for bulk data retrieval

---
+++ celery_app/connection.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from contextlib import contextmanager
+import os,dotenv, logging
+
+import dotenv
+
+dotenv.load_dotenv("../backend/.env")
+
+engine = None
+
+def get_engine():
+    global engine
+    if engine is None:
+        connection_url = f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432/{os.getenv('POSTGRES_DB')}"
+        engine = create_engine(connection_url)
+    return engine
+
+
+
+@contextmanager
+def get_connection():
+    engine = get_engine()
+    connection = engine.connect()
+    try:
+        yield connection
+    finally:
+        connection.close()
