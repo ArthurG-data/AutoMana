834b2b481ed5c2bbdb26cad3d56fcd8c4aa4d580
messi08 <guillaume.arthur1@gmail.com>
2025-10-15 09:59:49 -0400

    Add Scryfall data processing tasks and requirements
    - Introduced new Celery tasks for staging Scryfall set and card data.
    - Implemented file validation, size checks, and error handling in the new tasks.
    - Added a requirements.txt file for the celery_app.
    - Updated imports to include necessary modules and services for processing.

--- celery_app/celery_main_app.py
+++ celery_app/celery_main_app.py
@@ -2,10 +2,14 @@ import os, logging
 from celery import Celery
 from dotenv import load_dotenv
 
-
-
 celery_app = Celery('etl')
 celery_app.config_from_object('celeryconfig')
+
+try:
+    celery_app.connection().ensure_connection(max_retries=3)
+    logging.info("Connected to the message broker successfully.")
+except Exception as e:
+    logging.error(f"Failed to connect to the message broker: {e}")
 
 @celery_app.task
 def hello():
