bb400f4a4a6150e56f03149877bb3a6e67123246
messi08 <guillaume.arthur1@gmail.com>
2025-11-16 19:25:09 +1000

    log reader added, hardcoded locationt o be removed

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from backend.api.catalog import catalog_router
 from backend.api.users import user_router
 from backend.api.integrations import integrations_router
+from backend.api.logs.logs import log_router
 
 api_router = APIRouter(prefix="/api", tags=["API"])
     
 api_router.include_router(catalog_router)
 api_router.include_router(user_router)
 api_router.include_router(integrations_router)
+api_router.include_router(log_router)
