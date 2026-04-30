670e2467b3b4d476c25c84512add7574fc1b0a8e
messi08 <guillaume.arthur1@gmail.com>
2026-05-01 08:29:20 +1000

    style(db): wrap migration 19 in BEGIN/COMMIT to match project convention

--- src/automana/database/SQL/migrations/migration_19_widen_redirect_uri.sql
+++ src/automana/database/SQL/migrations/migration_19_widen_redirect_uri.sql
@@ -1,5 +1,9 @@
 -- migration_19_widen_redirect_uri.sql
 -- Widens app_info.redirect_uri from VARCHAR(50) to VARCHAR(255)
 -- so full ngrok/production URLs fit without truncation.
+BEGIN;
+
 ALTER TABLE app_integration.app_info
     ALTER COLUMN redirect_uri TYPE VARCHAR(255);
+
+COMMIT;
